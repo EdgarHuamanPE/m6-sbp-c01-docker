53810dc22690e4cbbe420d1362bc79f1a0183664
Edgar <edhuman@outlook.com>
2025-11-05 00:39:49 -0500

    add readme of docker project maven

--- 2_Dockerfile/HelloWorld.java
+++ 2_Dockerfile/HelloWorld.java
@@ -1,7 +1,17 @@
-
+import java.lang.Thread;
 
 public class HelloWorld{
     public static void main(String[] args){
         System.out.println("Hola mundo");
+        int cont=0;
+        while(true){
+             try {
+                cont++;
+                 System.out.println("mensaje #:"+cont+"Hello wordl..");
+                 Thread.sleep(1000);
+             } catch (Exception e){
+                 System.out.println("se interrumpi el hili...");
+             }
+        }
     }
 }
---
+++ 3_maven/README.md
@@ -0,0 +1,5 @@
+## Dockerfile con proyecto en maven
+
+    <img src="images/project.png" alt="Project Image "  width="600">
+
+
